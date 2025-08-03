3494812bb1104500786c5e55b4f54e99ec17b6f2
Oliiveralien <48074765+Oliiveralien@users.noreply.github.com>
2025-08-04 04:00:11 +0800

    Update inference_global.sh

--- inference_global.sh
+++ inference_global.sh
@@ -1,13 +1,14 @@
 export MODEL_NAME="CompVis/stable-diffusion-v1-4"
 export DATA_DIR='./test_datasets/'
+export Prompt="a photo of a S"
 
 CUDA_VISIBLE_DEVICES=0 python inference_global.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --test_data_dir=$DATA_DIR \
+  --template=$Prompt \
   --output_dir="./outputs/global_mapping"  \
   --suffix="object" \
   --token_index="0" \
-  --template="a photo of a S" \
   --global_mapper_path="./checkpoints/global_mapper.pt" \
   --seed=42
 
